17c9c4a3805c9c6126d244f2dc10953180e04946
Randy Sum <randy26smc@gmail.com>
2023-11-21 03:29:17 -0600

    se termino la fase de estilizar

--- resources/views/sell/form.blade.php
+++ resources/views/sell/form.blade.php
@@ -1,62 +1,65 @@
-<div class="container mt-5">
-    @if(count($errors) > 0)
-    <div class="alert alert-danger" role="alert">
-        <ul>
-            @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- ...otros encabezados... -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}">
+</head>
+<body>
+    <div class="wrapper">
+        @include('layouts.header')
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="card-title mb-4">Realizar Venta</h1>
+                            <form action="{{ route('sell.store') }}" method="post">
+                                @csrf
 
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <h1>Realizar Venta</h1>
-            <div class="card">
-                <div class="card-body">
-                    <h2 class="card-title mb-4">Ingreso</h2>
-                    <form action="{{ route('sell.store') }}" method="post">
-                        @csrf
+                                <div class="mb-3">
+                                    <label for="employee_id" class="form-label">Selecciona un Empleado:</label>
+                                    <select name="employee_id" id="employee_id" class="form-select" required>
+                                        @foreach($sellerEmployees as $employee)
+                                        <option value="{{ $employee->id }}">{{ $employee->name }} {{ $employee->last_name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
 
-                        <div class="mb-3">
-                            <label for="employee_id" class="form-label">Selecciona un Empleado:</label>
-                            <select name="employee_id" id="employee_id" class="form-select" required>
-                                @foreach($sellerEmployees as $employee)
-                                <option value="{{ $employee->id }}">{{ $employee->name }} {{ $employee->last_name }}</option>
-                                @endforeach
-                            </select>
+                                <div class="mb-3">
+                                    <label for="recycled_waste_inventory_id" class="form-label">Selecciona un Producto en Stock:</label>
+                                    <select name="recycled_waste_inventory_id" id="recycled_waste_inventory_id" class="form-select" required>
+                                        @foreach($recycledWasteInventories as $inventory)
+                                        <option value="{{ $inventory->id }}">{{ $inventory->name }} - Cantidad disponible: {{ $inventory->amount }} - Precio: {{$inventory->recycled_price}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="amount" class="form-label">Cantidad a Vender:</label>
+                                    <input type="text" name="amount" id="amount" class="form-control" required>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="date" class="form-label">Fecha:</label>
+                                    <input type="date" class="form-control" id="date" name="date" value="{{ old('date', now()->toDateString()) }}" required>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">Realizar Venta</button>
+                            </form>
                         </div>
-
-                        <div class="mb-3">
-                            <label for="recycled_waste_inventory_id" class="form-label">Selecciona un Producto en Stock:</label>
-                            <select name="recycled_waste_inventory_id" id="recycled_waste_inventory_id" class="form-select" required>
-                                @foreach($recycledWasteInventories as $inventory)
-                                <option value="{{ $inventory->id }}">{{ $inventory->name }} - Cantidad disponible: {{ $inventory->amount }} - Precio: {{$inventory->recycled_price}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        
-
-                        <div class="mb-3">
-                            <label for="amount" class="form-label">Cantidad a Vender:</label>
-                            <input type="text" name="amount" id="amount" class="form-control" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="date" class="form-label">Fecha:</label>
-                            <input type="date" class="form-control" id="date" name="date" value="{{ old('date', now()->toDateString()) }}" required>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Realizar Venta</button>
-                        
-                    </form>
+                    </div>
+                    <div class="mt-4 text-center">
+                <img src="https://procity.com.pe/wp-content/uploads/2020/06/00-reciclaje-que-desechos-rescatar-y-cuales-botar.jpg" alt="Venta de Reciclaje" class="img-fluid rounded w-100">
+            </div>
                 </div>
             </div>
+
+
+            
+
         </div>
     </div>
-
-    <div class="mt-4 text-center" style="max-width: 90%; overflow: hidden;">
-        <img src="https://procity.com.pe/wp-content/uploads/2020/06/00-reciclaje-que-desechos-rescatar-y-cuales-botar.jpg" alt="">
-    </div>
-</div>
+    @include('layouts.scripts')
+</body>
+</html>
--- resources/views/sell/index.blade.php
+++ resources/views/sell/index.blade.php
@@ -4,35 +4,41 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/base.css') }}">
-
 </head>
 
 <body>
     <div class="wrapper">
         @include('layouts.header')
 
+        <div class="container mt-5">
+            <h1 class="text-center mb-4">Registro de Ventas</h1>
 
-                <h1>Lista de Ventas</h1>
-
-                @if(count($errors) > 0)
+            @if(count($errors) > 0)
                 <div class="alert alert-danger" role="alert">
                     <ul>
                         @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
+                            <li>{{ $error }}</li>
                         @endforeach
                     </ul>
                 </div>
-                @endif
+            @endif
 
-                @if(count($sells) > 0)
-                <div class="tablas">
+            <p class="lead text-center">
+                Aquí puedes ver el registro de ventas.
+            </p>
+            <p class="lead text-center">
+                Puedes realizar una nueva venta presionando el boton de crear
+            </p>
+
+            @if(count($sells) > 0)
+
                     <table class="table">
                         <thead>
                             <tr>
-                                <th>ID</th>
+                                
                                 <th>Cantidad</th>
                                 <th>Precio de Venta</th>
-                                <th>Costro total</th>
+                                <th>Costo Total</th>
                                 <th>Fecha</th>
                                 <th>Empleado</th>
                                 <th>Producto en Stock</th>
@@ -40,29 +46,30 @@
                         </thead>
                         <tbody>
                             @foreach($sells as $sell)
-                            <tr>
-                                <td>{{ $sell->id }}</td>
-                                <td>{{ $sell->amount }}</td>
-                                <td>{{ $sell->sell_price }}</td>
-                                <td>{{ $sell->cost}}</td>
-                                <td>{{ $sell->date }}</td>
-                                <td>{{ $sell->employee->name }} {{ $sell->employee->last_name }}</td>
-                                <td>{{ $sell->recycledWasteInventory->name }} - Cantidad: {{ $sell->recycledWasteInventory->amount }}</td>
-                            </tr>
+                                <tr>
+                                    
+                                    <td>{{ $sell->amount }}</td>
+                                    <td>{{ $sell->sell_price }}</td>
+                                    <td>{{ $sell->cost }}</td>
+                                    <td>{{ $sell->date }}</td>
+                                    <td>{{ $sell->employee->name }} {{ $sell->employee->last_name }}</td>
+                                    <td>{{ $sell->recycledWasteInventory->name }} - Cantidad: {{ $sell->recycledWasteInventory->amount }}</td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
-                </div>
 
-                <a href="{{ route('sell.create') }}" class="btn btn-primary">Crear Venta</a>
+
                 <div class="d-grid mt-3">
-                            <a href="{{ route('sell.create') }}" class="btn btn-secondary">Crear Nueva Venta</a>
-                        </div>
-                @else
-                <p>No hay ventas registradas.</p>
-                @endif
-
-
+                    <a href="{{ route('sell.create') }}" class="btn btn-primary">Crear Nueva Venta</a>
+                </div>
+            @else
+                <p class="text-center">No hay ventas registradas.</p>
+                <div class="d-grid mt-3">
+                    <a href="{{ route('sell.create') }}" class="btn btn-primary">Crear Venta</a>
+                </div>
+            @endif
+        </div>
     </div>
 
     @include('layouts.scripts')
